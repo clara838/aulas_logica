febd79fc7179f2bb3ce6ef6380cd0fe2719e3767
clara munhoz <clara.munhoz@aluno.senai.br>
2025-04-08 10:20:24 -0300

    alteracao do hit.js

---
+++ exercicioss/git.js
@@ -0,0 +1,6 @@
+let comidas = ["lasanha", "pizza", "sushi", "hamburguer", "batata frita"]
+console.log('todas as comidas', comidas);
+
+console.log('todas as comidas'. comidas);
+console.sort()
+console.log('comidas ordenadas: ', comidas)
