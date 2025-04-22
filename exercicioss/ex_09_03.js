ea06011dae9504e2018d0f51c4529831f5c682d2
clara munhoz <clara.munhoz@aluno.senai.br>
2025-04-22 10:34:30 -0300

    .

--- exercicioss/ex_09_03.js
+++ exercicioss/ex_09_03.js
@@ -1,6 +1,6 @@
-function tabuada(numero)
+function tabuada(5, 10, 15, 20, 25, 30, 35, 40, 45, 50)
 let multiplicacao = `${tabuada}`
 return multiplicacao
 
-let resolucao = tabuada(5, 10, 15, 20, 25, 30, 35, 40, 45, 50)
+let resolucao = tabuada(5, 4, 6)
 console.log(resolucao)
